29ee440bc7f48513561693799bc28ce4aa4030d8
Matteo <matteorascioni@gmail.com>
2023-02-15 18:56:19 +0100

    adding the turtles data file

---
+++ turtles_data.py
@@ -0,0 +1,26 @@
+turtle_data = [
+    {
+        "color": "red",
+        "cordinates_y": 80
+    },
+    {
+        "color": "orange",
+        "cordinates_y": 50
+    },
+    {
+        "color": "yellow",
+        "cordinates_y": 15
+    },
+    {
+        "color": "green",
+        "cordinates_y": -15
+    },
+    {
+        "color": "blue",
+        "cordinates_y": -50
+    },
+    {
+        "color": "purple",
+        "cordinates_y": -80
+    },
+]
